cda496b074f16b1e3b5215de2f1bf78ec099eb7a
Subhashis Suara <subhashis.suara999@gmail.com>
2020-12-28 18:48:25 +0530

    Implement Selection Sort & Test File

--- sortingAlgos.py
+++ sortingAlgos.py
@@ -1,3 +1,8 @@
+# --------------------------
+#   Author: Subhashis Suara
+#   Student ID: UCSE19012
+# --------------------------
+
 def bubbleSort(inputArray): 
     size = len(inputArray)
     for i in range(size): 
@@ -6,3 +11,14 @@ def bubbleSort(inputArray):
                 temp = inputArray[j]
                 inputArray[j] = inputArray[j + 1]
                 inputArray[j + 1] = temp
+
+def selectionSort(inputArray):
+    size = len(inputArray)
+    for i in range(size):
+        minElemIndex = i
+        for j in range(i + 1, size):
+            if(inputArray[minElemIndex] > inputArray[j]):
+                minElemIndex = j
+        temp = inputArray[minElemIndex]
+        inputArray[minElemIndex] = inputArray[i]
+        inputArray[i] = temp
---
+++ test.py
@@ -0,0 +1,22 @@
+# --------------------------
+#   Author: Subhashis Suara
+#   Student ID: UCSE19012
+# --------------------------
+
+import sortingAlgos as sas
+
+# Testing all the algorithms
+
+inputArray = [5, 4, 3, 2, 1]
+
+# # Bubble Sort
+# sas.bubbleSort(inputArray)
+# print('Bubble Sort:')
+# for i in range(len(inputArray)):
+#     print(inputArray[i])
+
+# # Selection Sort
+# sas.selectionSort(inputArray)
+# print('Selection Sort:')
+# for i in range(len(inputArray)):
+#     print(inputArray[i])
